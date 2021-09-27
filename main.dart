44bdc653779803db3f2ac431dbf412c7c4dc4026
jesika10 <80150043+jesika10@users.noreply.github.com>
2021-09-27 21:03:29 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+class LayoutRow extends StatelessWidget {
+ Widget build(BuildContext context) {
+ return MaterialApp(
+ home: Scaffold(
+ appBar: AppBar(
+ flexibleSpace: SafeArea(
+ child: Container(
+ child: Column(
+ children: [
+ Row(
+ children: [
+ IconButton(
+ icon: Icon(Icons.menu),
+ tooltip: 'Navigation menu',
+ onPressed: null, // null disables the button
+ ),
+ Spacer(),
+ Text(
+ 'Kantin Politeknik Kampar',
+ textAlign: TextAlign.center,
+ ),
+ Spacer()
+ ],
+ )
+ ],
+ ),
+ )),
+ ),
+
+ body: Container(
+   child: Column(
+    children: [
+      Row(
+        children: [
+          Text(
+            'Kolom Pertama Baris Pertama',
+            textAlign: TextAlign.left,
+          ),
+          Spacer(),
+          Text(
+            'Kolom Kedua Baris Pertama',
+            textAlign: TextAlign.center,
+          ),
+          Spacer(),
+          Text(
+            'Kolom Ketiga Baris Pertama',
+            textAlign: TextAlign.right,
+          ),
+        ],
+      ),
+      Row(
+       children: [
+          Text(
+            'Kolom Pertama Baris Pertama',
+            textAlign: TextAlign.left,
+          ),
+          Spacer(),
+          Text(
+            'Kolom Kedua Baris kedua',
+            textAlign: TextAlign.center,
+          ),
+          Spacer(),
+          Text(
+            'Kolom Ketiga Baris kedua',
+            textAlign: TextAlign.right,
+          ),
+        ], 
+      )
+    ], 
+   ),
+ )
+ ));
+ }
+}
+void main() {
+ runApp(LayoutRow());
+}
